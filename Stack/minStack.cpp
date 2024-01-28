0b429a0e23733b2c0b0cc127fb772f5fd4f97ae2
datpham0412 <100574389+datpham0412@users.noreply.github.com>
2024-01-28 21:05:32 +1100

    minStack

--- Stack/minStack.cpp
+++ Stack/minStack.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <cassert>
 class MinStack{
     public:
     std::stack<int> array;
@@ -9,23 +10,44 @@ class MinStack{
     }
     void push(int val){
         array.push(val);
-        
+        if (minArray.empty() || val <= minArray.top()){
+            minArray.push(val);
+        }
 
     }
     void pop(){
-
+        if(minArray.top() == array.top()){
+            minArray.pop();
+        }
+        array.pop();
     }
     int top(){
+        return array.top();
 
     }
     int getMin(){
-        
+        return minArray.top();
     }
 
 };
 
 
 int main(){
-    std::cout <<"Hello World" <<std::endl;
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+
+    assert(minStack.getMin() == -3); // Should be -3
+    std::cout << "getMin() returned " << minStack.getMin() << " after push(-3), as expected." << std::endl;
+
+    minStack.pop();
+
+    assert(minStack.top() == 0); // Top should now be 0
+    std::cout << "top() returned " << minStack.top() << " after pop(), as expected." << std::endl;
+
+    assert(minStack.getMin() == -2); // Minimum should now be -2
+    std::cout << "getMin() returned " << minStack.getMin() << " after pop(), as expected." << std::endl;
+
     return 0;
 }

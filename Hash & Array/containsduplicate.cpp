770a2aec51d755c8a3ca1434dac7417c9a4b9a1a
Dat Pham <tiendatp@questps.com.au>
2024-02-14 09:39:57 +1100

    Heap allocation

--- Hash & Array/containsduplicate.cpp
+++ Hash & Array/containsduplicate.cpp
@@ -2,24 +2,30 @@
 #include <vector>
 #include <unordered_set>
 
-class Solution{
-    public:
-    bool containsDuplicate(std::vector<int>&nums){
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
         std::unordered_set<int> num_set;
-        for (int i : nums){
-            if(num_set.count(i)){
+        for (int i : nums) {
+            if (num_set.count(i)) {
                 return true;
-            }else{
+            } else {
                 num_set.insert(i);
             }
-        }return false;
+        }
+        return false;
     }
 };
 
-int main(){
-    Solution solution;
-    std::vector<int> nums = {1,1,2,3};
-    bool result = solution.containsDuplicate(nums);
-    std::cout<< result <<std::endl;
+int main() {
+    Solution* solution = new Solution(); // Dynamically allocate Solution object on heap
+    std::vector<int>* nums = new std::vector<int>({1, 1, 2, 3}); // Dynamically allocate vector on heap
+    
+    bool result = solution->containsDuplicate(*nums); // Dereference nums to pass by reference
+    std::cout << result << std::endl;
 
+    delete solution; // Clean up Solution object
+    delete nums; // Clean up vector
+
+    return 0;
 }

f7d41b31ea29396b49985fb308750e8379f81c13
Dat Pham <tiendatp@questps.com.au>
2024-03-05 17:01:07 +1100

    Insertion in Linnked List

--- test.cpp
+++ test.cpp
@@ -1,35 +1,14 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
-#include <unordered_set>
-#include <unordered_map>
-#include <climits>
 
+void swapValue(int &x, int &y){
+    int temp = x;
+    x = y; 
+    y = temp;
+}
 using namespace std;
-    
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        int left = 0;
-        int rigt = 0;
-
-        int minStart = 0;
-        int minLength = INT_MAX;
-
-        int counter = 0;
-        
-    }
-};
-int main() {
-    string s = "ADOBECODEBANC";
-    string t = "ABC";
-
-    Solution solution;
-    string result = solution.minWindow(s, t);
-
-    cout << "Minimum window substring: " << result << endl;
-
-    return 0;
+int main(){
+    int x = 10;
+    int y = 20;
+    swapValue(x, y);
+    cout << x << "," << y << endl;
 }

b3c2d2c08bbd6fa666ab953597b0d0a2984b4424
tiendatpQuest <tiendatp@questps.com.au>
2024-04-08 15:56:41 +1000

    Search in BST

---
+++ Trees/Binary Search Tree/Search in BST.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+
+using namespace std;
+
+class BST
+{
+public:
+    struct Node
+    {
+        int val;
+        Node *left;
+        Node *right;
+        Node(int value) : val(value), left(nullptr), right(nullptr) {}
+    };
+
+private:
+    Node *root;
+    Node *insertNode(Node *node, int value)
+    {
+        if (node == nullptr)
+        {
+            return new Node(value);
+        }
+        if (value < node->val)
+        {
+            node->left = insertNode(node->left, value);
+        }
+        else if (value > node->val)
+        {
+            node->right = insertNode(node->right, value);
+        }
+        return node;
+    }
+    Node *search(Node *node, int value)
+    {
+        // Base case: root is null or value is present at root
+        if (node == nullptr || node->val == value)
+        {
+            return node;
+        }
+        // Value is greater than root's data
+        if (node->val < value)
+        {
+            return search(node->right, value);
+        }
+
+        return search(node->left, value);
+    }
+
+public:
+    BST() : root(nullptr) {}
+    void insert(int value)
+    {
+        root = insertNode(root, value);
+    }
+    void inorderTraversal(Node *node)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        inorderTraversal(node->left);
+        cout << node->val << " ";
+        inorderTraversal(node->right);
+    }
+    void display()
+    {
+        cout << "Inorder traversal of the given tree: ";
+        inorderTraversal(root);
+        cout << endl;
+    }
+    bool search(int value)
+    {
+        Node *result = search(root, value);
+        return result != nullptr;
+    }
+};
+
+int main()
+{
+    BST *binarySearchTree = new BST();
+    binarySearchTree->insert(50);
+    binarySearchTree->insert(30);
+    binarySearchTree->insert(20);
+    binarySearchTree->insert(40);
+    binarySearchTree->insert(70);
+    binarySearchTree->insert(60);
+    binarySearchTree->insert(80);
+    if (binarySearchTree->search(20))
+    {
+        cout << "20 found in the BST" << endl;
+    }
+    else
+    {
+        cout << "20 not found in the BST" << endl;
+    }
+    if (binarySearchTree->search(25))
+    {
+        cout << "25 found in the BST" << endl;
+    }
+    else
+    {
+        cout << "25 not found in the BST" << endl;
+    }
+
+    binarySearchTree->display();
+    return 0;
+}
